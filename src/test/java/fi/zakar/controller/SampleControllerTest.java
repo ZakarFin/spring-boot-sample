d2e6658cc27e0fb1c7743de34402ecbc91d7c649
ZakarFin <zakarfin@gmail.com>
2014-10-26 18:04:47 +0200

    Tuned tests

--- src/test/java/fi/zakar/controller/SampleControllerTest.java
+++ src/test/java/fi/zakar/controller/SampleControllerTest.java
@@ -5,8 +5,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -16,6 +18,15 @@ public class SampleControllerTest {
     @Autowired
     SampleController ctrl;
 
+    @Autowired
+    ApplicationContext ctx;
+
+    @Test
+    public void testContextLoads() throws Exception {
+        assertNotNull(this.ctx);
+        assertTrue(this.ctx.containsBean("helloWorldService"));
+        assertTrue(this.ctx.containsBean("sampleSimpleApplication"));
+    }
     @Test
     public void testIndex() {
         final String output = ctrl.sayHello();
